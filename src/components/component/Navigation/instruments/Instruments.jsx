58bef4d8ec7aad0136602d6872056658aec0645e
ai-ahmad <ahmedovalmat8@gmail.com>
2024-02-25 17:06:29 +0500

    ahmad

--- src/components/component/Navigation/instruments/Instruments.jsx
+++ src/components/component/Navigation/instruments/Instruments.jsx
@@ -4,16 +4,24 @@ import './Instrument.css'; // Import CSS file
 const Instruments = () => {
   const [values, setValues] = useState('');
   const [qrCodeSrc, setQRCodeSrc] = useState('');
+  const [url,setUrl] = useState('')
   const [text,setText] = useState('')
 
   const handleInputChange = (event) => {
     setValues(event.target.value);
   }
 
+  const handleInputChange2 = (event) => {
+    setText(event.target.value);
+  }
+
   const handleGenerateQRCode = () => {
       fetch(`http://omonullo.uz:8006/api/${values}`).then(res => res.json()).then(data => {
-        setQRCodeSrc(data[0][0])
-        setText(data[1])
+        setQRCodeSrc(data)
+      })
+
+      fetch(`http://omonullo.uz:8006/text/${text}`).then(res => res.json()).then(data => {
+        setUrl(data)
       })
   }
   return (
@@ -25,9 +33,16 @@ const Instruments = () => {
         className="user-input"
         placeholder="Enter text for QR code"
       />
+      <input
+        type="text"
+        value={text}
+        onChange={handleInputChange2}
+        className="user-input1"
+        placeholder="Enter url brauser for QR code"
+      />
       <div className="qr-code-container">
       <img src={qrCodeSrc ? qrCodeSrc : ''}  className="qr-code-img" />
-      <p className='text_qr_code'>{text}</p>
+      <p className='text_qr_code'>{url}</p>
       </div>
       <button onClick={handleGenerateQRCode} className="generate-btn">
         Generate QR Code
