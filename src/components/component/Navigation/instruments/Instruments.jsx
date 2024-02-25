43230cfdd241b82f61f33698aa246fd08b494b5d
ai-ahmad <ahmedovalmat8@gmail.com>
2024-02-25 08:57:29 +0500

    ahmad

--- src/components/component/Navigation/instruments/Instruments.jsx
+++ src/components/component/Navigation/instruments/Instruments.jsx
@@ -4,6 +4,7 @@ import './Instrument.css'; // Import CSS file
 const Instruments = () => {
   const [values, setValues] = useState('');
   const [qrCodeSrc, setQRCodeSrc] = useState('');
+  const [text,setText] = useState('')
 
   const handleInputChange = (event) => {
     setValues(event.target.value);
@@ -12,6 +13,7 @@ const Instruments = () => {
   const handleGenerateQRCode = () => {
       fetch(`http://omonullo.uz:8006/api/${values}`).then(res => res.json()).then(data => {
         setQRCodeSrc(data[0][0])
+        setText(data[1])
       })
   }
   return (
@@ -25,6 +27,7 @@ const Instruments = () => {
       />
       <div className="qr-code-container">
       <img src={qrCodeSrc ? qrCodeSrc : ''}  className="qr-code-img" />
+      <p className='text_qr_code'>{text}</p>
       </div>
       <button onClick={handleGenerateQRCode} className="generate-btn">
         Generate QR Code
